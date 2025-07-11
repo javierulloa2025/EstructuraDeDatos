59b00fb817763373d83c43187c99f651bfec7823
francisco_ulloa <javier.ulloa@academicos.udg.mx>
2025-07-11 12:45:23 -0600

    nuevos algoritmos y medición de algoritmos

---
+++ 03A_Recursividad/04_ping_pong_time.py
@@ -0,0 +1,21 @@
+import time
+
+def ping(n):
+    if n == 0:
+        return
+    print("ping", n)
+    pong(n-1)
+
+def pong(n):
+    if n == 0:
+        return
+    print("pong", n)
+    ping(n-1)
+
+# Medición del tiempo
+inicio = time.perf_counter()
+
+ping(900)  # Solo necesitas llamar a uno, ya que se llaman mutuamente
+
+fin = time.perf_counter()
+print(f"\nTiempo de ejecución total: {fin - inicio:.6f} segundos")
---
+++ 03A_Recursividad/05_fibonnaci_time.py
@@ -0,0 +1,20 @@
+import time
+
+# Medición del tiempo
+inicio = time.perf_counter()
+
+#Sucesión de Fibonacci con recursividad
+def fib(n):
+    if n == 0 or n == 1:  #Caso base
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+    
+for numero in range(0, 39):
+    print(f"{fib(numero)}", end=" ")
+
+fin = time.perf_counter()
+print(f"\nTiempo de ejecución total: {fin - inicio:.6f} segundos")
+
+#38: 8.24 segundos
+#39: 19.64 segundos
