3b620b247d48c988570e644c199a1f2d1ac79aca
francisco_ulloa <javier.ulloa@academicos.udg.mx>
2025-08-04 17:01:24 -0600

    Algunos ejercicios

---
+++ 02_IntroEstructuras/03_Listas/12_listas_anidadas.py
@@ -0,0 +1,8 @@
+"""listas anidadas: dada la lista de abajo, se busca acceder (imprimir)
+los valores: 1, 2, g """
+
+lista_anidada = [1, "a", True, [1, 2, ["f","g", "h"]]]
+
+print(lista_anidada[0])
+print(lista_anidada[3][1])
+print(lista_anidada[3][2][1])
