586ef968f24dfe3ddb6ba75c984f2d606fb0c344
francisco_ulloa <javier.ulloa@academicos.udg.mx>
2025-08-06 19:08:07 -0600

    17_bogosort

---
+++ 04_Busqueda_Ordenamiento/18_sorted.py
@@ -0,0 +1,10 @@
+""" función sorted ()
+Retorna una nueva lista ordenada de un iterable (lista, tupla, cadena)
+no modifica el iterable
+Funciona con cualquier iterable
+"""
+
+tuplax = (5, 2, 1, 5, 7, 2)
+ordenado = sorted(tuplax)
+print(type(tuplax))
+print(type(ordenado))
