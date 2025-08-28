8d0fae750dc235b4800be479aaa1be932687834b
Francisco_Ulloa <javier.ulloa@academicos.udg.mx>
2025-08-28 13:14:27 -0600

    04_tuplas

---
+++ 02_IntroEstructuras/04_tuplas/00_calif.py
@@ -0,0 +1,9 @@
+reporte = (("Juan",[90,60,70,100]), 
+            ("Ana", [100, 85, 60, 85]),
+           ("Pedro", [90, 100, 100, 100]),
+           ("Ramón", [90, 95, 100, 100]),
+           ("María", [100, 85, 100, 90]))
+          
+for estudiante, calificaciones in reporte:
+    promedio = sum(calificaciones) / len(calificaciones)
+    print(f"{estudiante}, promedio: {promedio}")
